022b6c123ef9069e28c519865480415cf3c5e2ea
CassiaPerez <cassia.perez@grupocropfield.com.br>
2025-08-09 14:19:12 -0300

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,11 @@
 // src/main.tsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
-// (Opcional) se você tiver contextos:
-// import { AuthProvider } from "./contexts/AuthContext";
-// import { GamificationProvider } from "./contexts/GamificationContext";
-// import { Toaster } from "react-hot-toast";
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      {/* <AuthProvider>
-        <GamificationProvider> */}
-          <App />
-          {/* <Toaster position="top-right" /> */}
-        {/* </GamificationProvider>
-      </AuthProvider> */}
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
